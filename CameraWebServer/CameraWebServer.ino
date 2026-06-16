336886430c79b1f3629931e63b0fbe3a665f27f0
Abdussamed İLHAN <111380204+C-A-Y-L-A-K@users.noreply.github.com>
2026-06-16 17:37:10 +0300

    düzenleme
    Removed unnecessary conflict markers and cleaned up WiFi configuration section.

--- CameraWebServer/CameraWebServer.ino
+++ CameraWebServer/CameraWebServer.ino
@@ -18,10 +18,9 @@ extern void setServoPosition(uint32_t pulse_us);
 // ===========================
 // WiFi bilgilerinizi girin
 // ===========================
-<<<<<<< Updated upstream
+
 const char *ssid = "*****";          // Kamera ve uygulamanın aynı Ağa bağlı olması gerekmektedir.
 const char *password = "*****";      // Kameranın WiFi şifresi
-=======
 
 void startCameraServer();
 void setupLedFlash();
